8034c24391cb5a40cbb130cffcc3b6764cb78b6f
Croath <croathliu@gmail.com>
2014-01-18 01:49:24 +0800

    basic init

---
+++ SecretSwitch/SecretSwitch/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SecretSwitch
+//
+//  Created by croath on 1/18/14.
+//  Copyright (c) 2014 Croath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SecretSwitch/SecretSwitch/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SecretSwitch
+//
+//  Created by croath on 1/18/14.
+//  Copyright (c) 2014 Croath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
